58d8cea471b4dbb5ef4072ec2aa41d4ca0dc482c
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2024-04-18 10:50:08 +0530

    disease_prediction.py
    Gemini is integrated through env API key

--- dp/disease_prediction.py
+++ dp/disease_prediction.py
@@ -1,13 +1,96 @@
-import pandas as pd
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import textwrap, json
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+if not api_key:
+  print("Error: API_KEY environment variable not set")
+  exit(1)
+
+print(f"API Key: {api_key}")  # Consider hiding API keys for security
+
+app = Flask(__name__)
+
+@app.route('/api/test', methods=['GET'])
+def test():
+  data = {
+    "message": "Hello, World!",
+    "status": "success"
+  }
+  return jsonify(data)
+
+GOOGLE_API_KEY = api_key
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+prompt = '''You are a general doctor and doing basic diagnosis on patient data
+I am providing patient details, can you tell me what is the probably disease and medicines needed
+Data:
+gender = {}
+age = {}
+bodyTemp = {}
+pulseRate = {}
+respirationRate = {}
+bloodPressure = {}
+bloodOxygenLevel = {}
+weight = {}
+bloodGlucoseLevel = {}
+
+Output:
+probable diseases & medicines needed within 2 lines
+'''
+
+@app.route('/api/predict/', methods=['POST'])
+def predict():
+  if request.method == 'POST' and request.is_json:
+    data = request.get_json()
+    if len(data) == 9:
+      gender = data['gender']
+      age = data['age']
+      bodyTemp = data['bodyTemp']
+      pulseRate = data['pulseRate']
+      respirationRate = data['respirationRate']
+      bloodPressure = data['bloodPressure']
+      bloodOxygenLevel = data['bloodOxygenLevel']
+      weight = data['weight']
+      bloodGlucoseLevel = data['bloodGlucoseLevel']
+      
+      retries = 1
+      while True:
+        temp = prompt.format(*[i for i in data.values()]) # Generated Prompt 
+        response = model.generate_content(temp)
+        try:
+          if "sorry" not in response.text.lower().split() and "not" not in response.text.lower().split():
+            print("GEMINI WORKED")
+            break
+        except json.JSONDecodeError:
+          print(retries)
+          retries += 1
+          pass
+
+      return jsonify({
+        "prompt": temp,
+        "response": response.text
+      })
+    else:
+      return jsonify({'error': 'Missing required data in request body'}), 400
+  else:
+    return jsonify({'error': 'Invalid request method. Use POST'}), 405
+
+if __name__ == '__main__':
+  app.run(debug=True)
