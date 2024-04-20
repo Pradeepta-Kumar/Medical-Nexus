50612a65f8d5df70268c15f8bd4b23d9a76f9116
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2024-04-20 23:14:57 +0530

    database schema

--- backend/controllers/HealthData.js
+++ backend/controllers/HealthData.js
@@ -12,35 +12,41 @@ const healthData = async (req, res) => {
       bloodOxygenLevel,
       weight,
       bloodGlucoseLevel,
+      userEmail
     } = req.body;
-    if (
-      !gender ||
-      !age ||
-      !bodyTemp ||
-      !pulseRate ||
-      !respirationRate ||
-      !bloodPressure ||
-      !bloodOxygenLevel ||
-      !weight ||
-      !bloodGlucoseLevel
-    ) {
+    if (!gender || !age || !bodyTemp || !pulseRate || !respirationRate || !bloodPressure || !bloodOxygenLevel || !weight || !bloodGlucoseLevel) {
       return res.status(401).json({
         message: "You can't leave any data",
         success: false,
       });
     }
+    const newData = {
+      gender: gender,
+      age: age,
+      bodyTemp: bodyTemp,
+      pulseRate: pulseRate,
+      respirationRate: respirationRate,
+      bloodPressure: bloodPressure,
+      bloodOxygenLevel: bloodOxygenLevel,
+      weight: weight,
+      bloodGlucoseLevel: bloodGlucoseLevel,
+      date: new Date(),
+      result: 'temp'  // make an API call to flask
+    };
 
-    await HealthData.create({
-      gender,
-      age,
-      bodyTemp,
-      pulseRate,
-      respirationRate,
-      bloodPressure,
-      bloodOxygenLevel,
-      weight,
-      bloodGlucoseLevel,
-    });
+    const healthData = await HealthData.findOne({ userEmail: userEmail });
+    if (!healthData) {
+      // If no health data found for the user, create a new document
+      const newUserHealthData = new HealthData({
+          userEmail: userEmail,
+          data: [newData]
+      });
+      await newUserHealthData.save();
+    } else {
+      healthData.data.push(newData);
+      await healthData.save();
+    }
+    
     return res.status(200).json({
       message: "Data added successfully!",
       success: true,
--- backend/models/healthData.js
+++ backend/models/healthData.js
@@ -1,42 +1,58 @@
 const mongoose = require("mongoose");
 
 const healthDataSchema = mongoose.Schema({
-    gender: {
-        type: String, 
+    userEmail: {
+        type: String,
         required: true
     },
-    age: {
-        type: Number,
-        required: true,
-    },
-    bodyTemp: {
-        type: Number,
-        required: true,
-    },
-    pulseRate: {
-        type: Number,
-        required: true,
-    },
-    respirationRate: {
-        type: Number,
-        required: true,
-    },
-    bloodPressure: {
-        type: Number,
-        required: true,
-    },
-    bloodOxygenLevel: {
-        type: Number,
-        required: true,
-    },
-    weight: {
-        type: Number,
-        required: true,
-    },
-    bloodGlucoseLevel: {
-        type: Number,
-        required: true,
-    }
+    data: [
+        {
+            gender: {
+                type: String, 
+                required: true
+            },
+            age: {
+                type: Number,
+                required: true,
+            },
+            bodyTemp: {
+                type: Number,
+                required: true,
+            },
+            pulseRate: {
+                type: Number,
+                required: true,
+            },
+            respirationRate: {
+                type: Number,
+                required: true,
+            },
+            bloodPressure: {
+                type: Number,
+                required: true,
+            },
+            bloodOxygenLevel: {
+                type: Number,
+                required: true,
+            },
+            weight: {
+                type: Number,
+                required: true,
+            },
+            bloodGlucoseLevel: {
+                type: Number,
+                required: true,
+            },
+            date: {
+                type: Date,
+                required: true,
+            },
+            result: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 });
 
 const HealthData = mongoose.model("HealthData", healthDataSchema);
